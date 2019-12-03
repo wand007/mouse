01a2dc1e917fcefb712b10bfa904a3e49592060a
李东东 <lidongdong@gxcards.com>
2019-12-03 15:53:52 +0800

    Commit add test feign

--- rotor-admin/src/main/java/com/mouse/admin/controller/ResourcesController.java
+++ rotor-admin/src/main/java/com/mouse/admin/controller/ResourcesController.java
@@ -1,10 +1,15 @@
 package com.mouse.admin.controller;
 
 import com.mouse.admin.base.BaseController;
+import com.mouse.api.feign.ResourcesFeign;
+import com.mouse.core.base.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 
 /**
  * @author ; lidongdong
@@ -17,5 +22,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("resources")
 public class ResourcesController extends BaseController {
 
+    @Autowired
+    private ResourcesFeign resourcesFeign;
 
+    /**
+     * 分页查询商品列表
+     *
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @ResponseBody
+    @GetMapping(value = "findPage")
+    public R findPage(
+            @Min(value = 0, message = "必须从0页开始")
+            @RequestParam(name = "pageNum", defaultValue = "0", required = false) Integer pageNum,
+            @Min(value = 1, message = "每页必须大于1")
+            @Max(value = 300, message = "每页必须小于300")
+            @RequestParam(name = "pageSize", defaultValue = "20", required = false) Integer pageSize) {
+        return resourcesFeign.findPage(pageNum, pageSize);
+    }
 }
