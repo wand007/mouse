f98670938c8ba0ede387f96caebed439bec7f471
李东东 <lidongdong@gxcards.com>
2020-05-10 18:59:27 +0800

    格式化

--- rotor-api/src/main/java/com/mouse/api/system/SystemInfoPrinter.java
+++ rotor-api/src/main/java/com/mouse/api/system/SystemInfoPrinter.java
@@ -26,13 +26,14 @@ public class SystemInfoPrinter {
 
     private static void setMaxSize(Map<String, String> infos) {
         for (Map.Entry<String, String> entry : infos.entrySet()) {
-            if (entry.getValue() == null)
+            if (entry.getValue() == null) {
                 continue;
-
+            }
             int size = entry.getKey().length() + entry.getValue().length();
 
-            if (size > maxSize)
+            if (size > maxSize) {
                 maxSize = size;
+            }
         }
 
         maxSize = maxSize + 30;
@@ -60,8 +61,9 @@ public class SystemInfoPrinter {
     }
 
     private static void printLine(String head, String line) {
-        if (line == null)
+        if (line == null) {
             return;
+        }
 
         if (head.startsWith(CREATE_PART_COPPER)) {
             logger.info("");
